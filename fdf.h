279dee7600a92c9111c8c2022009d71586a4e943
Sharfy Adamantine <sadamant@student.42.us.org>
2017-11-17 14:54:53 +0100

    first plan

---
+++ fdf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/17 11:41:14 by sadamant          #+#    #+#             */
+/*   Updated: 2017/11/17 13:14:50 by sadamant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h> //open
+#include <stdlib.h> //malloc, free
+#include <string.h> //strerror
+#include <math.h>
+#include "mlx.h"
+#include "libft.h"
+
+# define TILE_WIDTH 20
+# define TILE_HEIGHT 20
